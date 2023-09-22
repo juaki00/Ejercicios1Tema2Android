56b80482790823ae1515c16769c939aa51725499
JoaquinRomeroSanchez <juaki00@gmail.com>
2023-09-22 12:45:51 +0200

    ejercicio 3 treminado

--- app/src/main/java/com/example/myapplication/Ej3.kt
+++ app/src/main/java/com/example/myapplication/Ej3.kt
@@ -1,2 +1,25 @@
 package com.example.myapplication
 
+fun main() {
+    var distancia = 10.0
+    var disponible = false
+    while (distancia > 0.5 || !disponible) {
+        println("Introduce la distancia en kilometros")
+        distancia = readLine()!!.toDouble()
+        println("Introduce la disponibilidad (true = disponible , false = no disponible)")
+        disponible = readLine()!!.toBoolean()
+        println("La distacia es $distancia y la disponibilidad es $disponible")
+
+        if (distancia <= 0.5) {
+            if (disponible)
+                println("Listo para iniciar recorrido!")
+            else
+                println("Conductor cercano pero no disponible!")
+        } else {
+            if (disponible)
+                println("Conductor disponible pero muy lejos,aplicarán tarifas más altas!")
+            else
+                println("No hay conductores disponibles!")
+        }
+    }
+}
